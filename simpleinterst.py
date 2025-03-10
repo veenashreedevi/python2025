df658743b2a75bb169f26ffc8707aaf0f8d06970
veenashreedevi <veenasiddu328@gmail.com>
2025-03-10 16:03:44 +0530

    Create simpleinterst.py

---
+++ simpleinterst.py
@@ -0,0 +1,5 @@
+principalamount = int(input("enter amount"))
+years=int(input("enter years"))
+rateofintrest=float(input("enter interst per annum"))
+simpleintrest=principalamount*years*rateofintrest/100
+print(simpleintrest)
