7307c717ae02a427eed12e85d351d50126786838
veenashreedevi <veenasiddu328@gmail.com>
2025-03-11 11:43:55 +0530

    Create number.py

---
+++ number.py
@@ -0,0 +1,13 @@
+num1=float(input("Enter frist number:"))
+num2=float(input("Enter second number:"))
+operation =input("Enter opertaion(+,-,*,/):")
+if operation == '+':
+  print("Result:",num1+num2)
+ elif operation == '-':
+  print("Result:", num1-num2)
+ elif operation =='*':
+  print("Result:",num1*num2)
+ elif operation =='/':
+  print("reslut:",num1/num2)
+ else
+  print("Invalid Operation")
