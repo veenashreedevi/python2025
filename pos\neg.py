a2df6ae78b264457cef5fedba524e92bc3d60163
veenashreedevi <veenasiddu328@gmail.com>
2025-03-11 11:53:23 +0530

    Create pos\neg.py

---
+++ pos\neg.py
@@ -0,0 +1,7 @@
+number = float(input("Enter a number: "))
+if number > 0:
+    print(f"{number} is a positive number.")
+elif number < 0:
+    print(f"{number} is a negative number.")
+else:
+    print(f"{number} is zero.")
