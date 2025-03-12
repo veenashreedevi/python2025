ec39ba147d26acd94e2ef79c8f6a7fa588234c44
veenashreedevi <veenasiddu328@gmail.com>
2025-03-12 14:43:38 +0530

    Create reversfun.py

---
+++ reversfun.py
@@ -0,0 +1,6 @@
+def reverse_string(input_string):
+    return input_string[::-1]
+input_str = "Hello, World!"
+reversed_str = reverse_string(input_str)
+print(f"Reversed string: {reversed_str}")
+print("veena-ec072")
