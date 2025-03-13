7996e3afbf0ed1ae4d61d8f759bd50656032db90
veenashreedevi <veenasiddu328@gmail.com>
2025-03-13 14:43:41 +0530

    Create meger.py

---
+++ meger.py
@@ -0,0 +1,6 @@
+list1 = [10, 20, 30, 40, 50]
+list2 = [30, 40, 50, 60, 70]
+merged_list = list1 + list2
+unique_list = list(set(merged_list))
+print("Merged List without Duplicates:", unique_list)
+print("veenaEC-072")
