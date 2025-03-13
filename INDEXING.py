48cabf3fed8e34857cb3c483f1331cef48100dd1
veenashreedevi <veenasiddu328@gmail.com>
2025-03-13 14:15:33 +0530

    Create INDEXING.py

---
+++ INDEXING.py
@@ -0,0 +1,7 @@
+numbers = [10, 20, 30, 40, 50]
+first_element = numbers[0]
+last_element = numbers[-1]
+middle_element = numbers[len(numbers) // 2]
+print("First element:", first_element)
+print("Last element:", last_element)
+print("Middle element:", middle_element)
