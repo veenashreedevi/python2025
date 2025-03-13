d13d2fcfb1427b24089401b7c32b17bb3b9baa34
veenashreedevi <veenasiddu328@gmail.com>
2025-03-13 12:48:11 +0530

    Create oops.py

---
+++ oops.py
@@ -0,0 +1,13 @@
+#oops
+from typing_extensions import AsyncGenerator
+from os import name
+class Person:
+  def _init_(self,name,age):
+    self.name = name
+    self.age = Age
+
+    def greet(self):
+      print(f"Hello,my name is{self.name}and I am{self.age}years old.")
+
+      person1 = person("Alice",25)
+      personl.greet()
