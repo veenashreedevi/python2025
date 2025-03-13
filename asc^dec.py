3697cf0a9277e7bc105e55c8d278082200ed4b92
veenashreedevi <veenasiddu328@gmail.com>
2025-03-13 14:27:55 +0530

    Create asc^dec.py

---
+++ asc^dec.py
@@ -0,0 +1,5 @@
+numbers = [10, 50, 30, 20, 40]
+numbers_ascending = sorted(numbers)
+numbers_descending = sorted(numbers, reverse=True)
+print("List in Ascending Order:", numbers_ascending)
+print("List in Descending Order:", numbers_descending)
