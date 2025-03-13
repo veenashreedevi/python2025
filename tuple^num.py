be176b905b5239cf8f52b3b44711f01c73052210
veenashreedevi <veenasiddu328@gmail.com>
2025-03-13 14:50:15 +0530

    Create tuple^num.py

---
+++ tuple^num.py
@@ -0,0 +1,11 @@
+my_tuple = (10, 20, 30, 40, 50)
+first_element = my_tuple[0]
+second_element = my_tuple[1]
+third_element = my_tuple[2]
+fourth_element = my_tuple[3]
+fifth_element = my_tuple[4]
+print("First Element:", first_element)
+print("Second Element:", second_element)
+print("Third Element:", third_element)
+print("Fourth Element:", fourth_element)
+print("Fifth Element:", fifth_element)
