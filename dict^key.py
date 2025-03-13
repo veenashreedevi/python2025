fefbfa44c469417bf726fcac7c76e11b942a617a
veenashreedevi <veenasiddu328@gmail.com>
2025-03-13 15:01:20 +0530

    Create dict^key.py

---
+++ dict^key.py
@@ -0,0 +1,16 @@
+my_dict = {
+    "name": "John",
+    "age": 25,
+    "city": "New York",
+    "job": "Engineer"
+}
+
+name = my_dict["name"]
+age = my_dict["age"]
+city = my_dict["city"]
+job = my_dict["job"]
+print("Name:", name)
+print("Age:", age)
+print("City:", city)
+print("Job:", job)
+print("veenaEC-072")
