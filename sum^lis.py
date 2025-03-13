1a382abfe38ccfc15748bff05047045524eb3909
veenashreedevi <veenasiddu328@gmail.com>
2025-03-13 14:33:08 +0530

    Create sum^lis.py

---
+++ sum^lis.py
@@ -0,0 +1,7 @@
+# Function to calculate the sum of list elements
+def calculate_sum(numbers):
+    total = sum(numbers)  
+    return total
+numbers = [10, 20, 30, 40, 50]
+result = calculate_sum(numbers)
+print("Sum of List Elements:", result)
